383cf73f8a6a40430032a5d9d40dd206a19d7bf6
Qv1ko <vicgarmur947@gmail.com>
2022-11-16 21:02:17 +0100

    modification of the icon module installations

--- autPosh.ps1
+++ autPosh.ps1
@@ -9,14 +9,17 @@ Write-Host "Options: `n  1) Installing `n  2) Updating `n  3) Exit"
 $options=Read-Host "Select option"
 $theme="catppuccin_macchiato"
 
+#Function
 switch ($options) {
     1 {
         Write-Host "Install Windows Terminal and PowerShell"
         winget install --id=Microsoft.WindowsTerminal -e
         winget install --id Microsoft.Powershell --source winget
+        
         Write-Host "Install Firacode font"
         Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
         choco install firacode -y
+        
         Write-Host "Install and configure OhMyPosh"
         winget install JanDeDobbeleer.OhMyPosh -s winget
         New-Item -Path $PROFILE -Type File -Force
@@ -24,7 +27,12 @@ switch ($options) {
         $theme=Read-Host "Write your favourite theme"
         Write-Output "oh-my-posh init pwsh --config `"$env:POSH_THEMES_PATH\$theme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $PROFILE
         . $PROFILE
+        
+        Write-Host "Install icons module"
+        Install-PackageProvider NuGet -Force;
+        Set-PSRepository PSGallery -InstallationPolicy Trusted
         Install-Module -Name Terminal-Icons -Repository PSGallery
+        
         Clear-Host
         (Invoke-WebRequest "https://raw.githubusercontent.com/kiedtl/winfetch/master/winfetch.ps1" -UseBasicParsing).Content.Remove(0,1) | Invoke-Expression
         Start-Sleep(10)

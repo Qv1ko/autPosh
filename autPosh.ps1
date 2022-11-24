d228ee56ebf964d1f2b05a186e1863e7e07de1ad
Qv1ko <vicgarmur947@gmail.com>
2022-11-24 23:32:22 +0100

    Minimising if and eliminating global variables

--- autPosh.ps1
+++ autPosh.ps1
@@ -4,84 +4,55 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 	Exit
 }
 
-#Gloval variables
-Write-Host "Options: `n  1) Installing `n  2) Updating `n  3) Exit"
-$options=Read-Host "Select option"
-$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-
 #Function
-switch ($options) {
-    1 {
-        $newpath = "$oldpath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
-        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
+$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+$newpath = "$oldpath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
 
-        if (Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\winget.exe){
-            'Winget already installed'
-        }  
-        else{
-            Write-Host "Install Winget"
-            Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-            $wiid = (Get-Process AppInstaller).Id
-	        Wait-Process -Id $wiid
-            . $PROFILE
-        }
-
-        if (Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe){
-            'Windows Terminal already installed'
-        }  
-        else{
-            Write-Host "Install Windows Terminal"
-            winget install --id=Microsoft.WindowsTerminal -e
-            . $PROFILE
-        }
-
-        if (Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe){
-            'PowerShell already installed'
-        }  
-        else{
-            Write-Host "Install PowerShell"
-            winget install --id Microsoft.Powershell --source winget
-            . $PROFILE
-        }
-        
-        Write-Host "Install Firacode font"
-        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        choco install firacode -y
-
-        Write-Host "Terminal configuration"
-        $terminalpath = Get-ChildItem -Path "$env:USERPROFILE\AppData\Local\Packages\" -Name "Microsoft.WindowsTerminal_*" -Directory
-        Move-Item -Path .\settings.json -Destination $env:USERPROFILE\AppData\Local\Packages\$terminalpath\LocalState\ -PassThru
-        
-        Write-Host "Install and configure OhMyPosh"
-        winget install JanDeDobbeleer.OhMyPosh -s winget
-        New-Item -Path $PROFILE -Type File -Force
-        Start-Process "https://ohmyposh.dev/docs/themes"
-        $theme=Read-Host "Write your favourite theme"
-        Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$theme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $PROFILE
-        . $PROFILE
-        
-        Write-Host "Install icons module"
-        Install-PackageProvider NuGet -Force;
-        Set-PSRepository PSGallery -InstallationPolicy Trusted
-        Install-Module -Name Terminal-Icons -Repository PSGallery
-        . $PROFILE
-        
-        Write-Host "`nFinished`n"
-        Start-Sleep(5)
-    }
-    2 {
-        winget upgrade Microsoft.WindowsTerminal -s winget -h --accept-package-agreements --accept-source-agreements
-        winget upgrade Microsoft.Powershell -s winget -h --accept-package-agreements --accept-source-agreements
-        winget upgrade JanDeDobbeleer.OhMyPosh -s winget -h --accept-package-agreements --accept-source-agreements
-        Start-Sleep(2)
-        Clear-Host; (Invoke-WebRequest "https://raw.githubusercontent.com/kiedtl/winfetch/master/winfetch.ps1" -UseBasicParsing).Content.Remove(0,1) | Invoke-Expression
-        Start-Sleep(5)
-    }
-    3 {
-        Write-Host "`nExit...`n"
-        Start-Sleep(3)
-    }
-    Default {
-        Write-Host "`n!Select valid option`n"
-    }
+if (Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\winget.exe){
+} else{
+    Write-Host "Install Winget"
+    Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+    $wiid = (Get-Process AppInstaller).Id
+    Wait-Process -Id $wiid
+    . $PROFILE
 }
+
+if (Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe){
+} else{
+    Write-Host "Install Windows Terminal"
+    winget install --id=Microsoft.WindowsTerminal -e
+    . $PROFILE
+}
+
+if (Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe){
+} else{
+    Write-Host "Install PowerShell"
+    winget install --id Microsoft.Powershell --source winget
+    . $PROFILE
+}
+
+Write-Host "Install Firacode font"
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install firacode -y
+
+Write-Host "Terminal configuration"
+$terminalpath = Get-ChildItem -Path "$env:USERPROFILE\AppData\Local\Packages\" -Name "Microsoft.WindowsTerminal_*" -Directory
+Copy-Item -Path .\settings.json -Destination $env:USERPROFILE\AppData\Local\Packages\$terminalpath\LocalState\ -PassThru
+
+Write-Host "Install and configure OhMyPosh"
+winget install JanDeDobbeleer.OhMyPosh -s winget
+New-Item -Path $PROFILE -Type File -Force
+Start-Process "https://ohmyposh.dev/docs/themes"
+$theme=Read-Host "Write your favourite theme"
+Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$theme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $PROFILE
+. $PROFILE
+
+Write-Host "Install icons module"
+Install-PackageProvider NuGet -Force;
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module -Name Terminal-Icons -Repository PSGallery
+. $PROFILE
+
+Write-Host "`nFinished`n"
+Start-Sleep(5)

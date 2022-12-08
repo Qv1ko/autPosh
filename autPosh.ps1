b41d74e9b24fe95fd30f2975c21f056d3d262548
Qv1ko <vicgarmur947@gmail.com>
2022-12-08 23:38:33 +0100

    Cambio en la nomenclatura

--- autPosh.ps1
+++ autPosh.ps1
@@ -5,29 +5,29 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 }
 
 #path adding
-$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$newpath = "$oldpath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
+$OldPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+$NewPath = "$OldPath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $NewPath
 
 #winget install condition
 if (!(Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\winget.exe)) {
     Write-Host "Install Winget"
     Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-    $wiid = (Get-Process AppInstaller).Id
-    Wait-Process -Id $wiid
+    $InstallId = (Get-Process AppInstaller).Id
+    Wait-Process -Id $InstallId
     . $PROFILE
 }
 
 #windows terminal install condition
 if (!(Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe)) {
     Write-Host "Install Windows Terminal"
-    winget install --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
+    winget install --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements cf
 }
 
 #powershell install condition
 if (!(Test-Path $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe)) {
     Write-Host "Install PowerShell"
-    winget install --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
+    winget install --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements cf
 }
 
 #terminal font installing
@@ -44,8 +44,8 @@ Write-Host "Install and configure OhMyPosh"
 winget install JanDeDobbeleer.OhMyPosh -s winget
 New-Item -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Type File -Force
 Start-Process "https://ohmyposh.dev/docs/themes"
-$theme=Read-Host "Write your favourite theme"
-Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$theme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+$PoshTheme=Read-Host "Write your favourite theme"
+Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
 #module icons installing
 Write-Host "Install icons module"
@@ -56,4 +56,3 @@ Install-Module -Name Terminal-Icons -Repository PSGallery
 #exit
 wt -w 0 nt pwsh -NoExit -c {(Invoke-WebRequest "https://raw.githubusercontent.com/kiedtl/winfetch/master/winfetch.ps1" -UseBasicParsing).Content.Remove(0,1) | Invoke-Expression}
 Read-Host "Pulse Enter para salir"
-Exit
--- themPosh.ps1
+++ themPosh.ps1
@@ -1,4 +1,4 @@
 Start-Process "https://ohmyposh.dev/docs/themes"
-$theme=Read-Host "Write your favourite theme"
-Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$theme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+$PoshTheme=Read-Host "Write your favourite theme"
+Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme`.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 wt -w 0 nt pwsh -NoExit

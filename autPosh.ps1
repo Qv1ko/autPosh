2ff2a63ce68394c74df649b7020d6e41dc5835f1
Qv1ko <vicgarmur947@gmail.com>
2023-02-15 22:24:26 +0100

    Cambios en la elevación de privilegios y pruebas

--- autPosh.ps1
+++ autPosh.ps1
@@ -1,93 +1,62 @@
-#Administrator PowerShell
+#Elevation of script privileges
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 	Exit
 }
 
-# param(
-#     [Parameter(Mandatory=$false)]
-#     [switch]$shouldAssumeToBeElevated,
-
-#     [Parameter(Mandatory=$false)]
-#     [String]$workingDirOverride
-# )
-# if(-not($PSBoundParameters.ContainsKey('workingDirOverride'))) {
-#     $workingDirOverride = (Get-Location).Path
-# }
-# function Test-Admin {
-#     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-#     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-# }
-# if ((Test-Admin) -eq $false)  {
-#     if ($shouldAssumeToBeElevated) {
-#         Write-Output "Elevating did not work :("
-#     } else {
-#         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -shouldAssumeToBeElevated -workingDirOverride "{1}"' -f ($myinvocation.MyCommand.Definition, "$workingDirOverride"))
-#     }
-#     exit
-# }
-# Set-Location "$workingDirOverride"
-
-
 $Option=Read-Host "(1) Install, (2) Theme selector`nSelect option"
 if($Option==1) {
-    #path adding
+    #Path configuration
     $OldPath = (Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).path
     $NewPath = "$OldPath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
     Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH -Value $NewPath
-
-    #winget install condition
+    #Winget install condition
     if (!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\winget.exe")) {
         Write-Host "Install Winget"
         Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
         $InstallId = (Get-Process AppInstaller).Id
         Wait-Process -Id $InstallId
     }
-
-    #windows terminal install condition
+    #Windows terminal install condition
     if (!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe")) {
         Write-Host "Install Windows Terminal"
         winget install --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
     }
-
-    #powershell install condition
+    #Powershell install condition
     if (!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe")) {
         Write-Host "Install PowerShell"
         winget install --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
     }
-
-    #terminal font installing
+    #Terminal font installation
     Write-Host "Install Mononoki font"
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
     choco install nerd-fonts-mononoki -y
-
-    #terminal configuration
+    #Terminal configuration
     Write-Host "Terminal configuration"
     Copy-Item -Path "$PSCommandPath\..\settings.json" -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\" -Force
-
-    #ohmyposh install and configuration
+    #OhMyPosh configuration and installation
     Write-Host "Install and configure OhMyPosh"
     winget install JanDeDobbeleer.OhMyPosh -s winget
     New-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -ItemType File -Force
     Start-Process "https://ohmyposh.dev/docs/themes"
     $PoshTheme=Read-Host "Write your favourite theme"
     Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-
-    #module icons installing
+    #Icons module installation
     Write-Host "Install icons module"
     Install-PackageProvider NuGet -Force
     Set-PSRepository PSGallery -InstallationPolicy Trusted
     Install-Module -Name Terminal-Icons -Repository PSGallery
-
-    #exit
+    #Exit
     . $PROFILE
     wt -w 0 nt -NoExit
 } elseif($Option==2) {
+    #OhMyPosh theme configuration
     Start-Process "https://ohmyposh.dev/docs/themes"
     $PoshTheme=Read-Host "Write your favourite theme"
     Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
     wt -w 0 nt -NoExit
 } else {
+    #Error exit
     Write-Host "Please select valid option"
     Start-Sleep(5)
 }

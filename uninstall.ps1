84639001210b081a86cb529e7fb74bd2a2fbf3a4
Qv1ko <vicgarmur947@gmail.com>
2023-03-04 20:53:05 +0100

    Coding uninstall script

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,48 +1,23 @@
+#Elevation of script privileges
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 	Exit
 }
-
-#path adding
-$OldPath = (Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).path
-$NewPath = "$OldPath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
-Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH -Value $NewPath
-
-#windows terminal uninstall condition
-if (Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe") {
-    Write-Host "Uninstall Windows Terminal"
-    winget uninstall --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
-}
-
-#powershell uninstall condition
-if (Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe") {
-    Write-Host "Uninstall PowerShell"
-    winget uninstall --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
-}
-
-#terminal font installing
-Write-Host "Install Mononoki font"
+#Icons module uninstallation
+Uninstall-Module -Name Terminal-Icons -Repository PSGallery
+#OhMyPosh uninstallation
+winget uninstall JanDeDobbeleer.OhMyPosh -s winget
+Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force
+#Font uninstallation
 #Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
 choco uninstall nerd-fonts-mononoki -y
-
-#terminal configuration
-Write-Host "Terminal configuration"
-Copy-Item -Path "$PSCommandPath\..\settings.json" -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\" -Force
-
-#ohmyposh install and configuration
-Write-Host "Install and configure OhMyPosh"
-winget install JanDeDobbeleer.OhMyPosh -s winget
-New-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -ItemType File -Force
-Start-Process "https://ohmyposh.dev/docs/themes"
-$PoshTheme=Read-Host "Write your favourite theme"
-Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-
-#module icons installing
-Write-Host "Install icons module"
-Install-PackageProvider NuGet -Force
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module -Name Terminal-Icons -Repository PSGallery
-
-#exit
-. $PROFILE
-wt -w 0 nt -NoExit
+#Powershell uninstall condition
+if (Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe") {
+    winget uninstall --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
+}
+#Deleting terminal configuration
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
+#Windows terminal uninstall condition
+if (Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe") {
+    winget uninstall --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
+}

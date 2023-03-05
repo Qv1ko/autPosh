89e609c375260e76600636c9e6d6e7fc068fc462
Qv1ko <vicgarmur947@gmail.com>
2023-03-05 01:51:41 +0100

    Correción de errores

--- uninstall.ps1
+++ uninstall.ps1
@@ -5,19 +5,12 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 }
 #Icons module uninstallation
 Uninstall-Module -Name Terminal-Icons
-Uninstall-Module -Name Terminal-Icons -Repository PSGallery
 #OhMyPosh uninstallation
 winget uninstall JanDeDobbeleer.OhMyPosh -s winget
 Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force
 #Font uninstallation
 choco uninstall nerd-fonts-mononoki -y
 #Powershell uninstall condition
-if (Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe") {
-    winget uninstall --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
-}
-#Deleting terminal configuration
-Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
+winget uninstall --id Microsoft.Powershell --source winget
 #Windows terminal uninstall condition
-if (Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe") {
-    winget uninstall --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
-}
+winget uninstall --id=Microsoft.WindowsTerminal -e

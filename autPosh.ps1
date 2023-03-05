c0fbd7266794b6fca5783f4a33f92577554032d9
Qv1ko <vicgarmur947@gmail.com>
2023-03-06 00:05:56 +0100

    Añadido elseif y breaks

--- autPosh.ps1
+++ autPosh.ps1
@@ -8,23 +8,23 @@ do {
     $Option=Read-Host "(1) Install, (2) Theme selector (3) Uninstall (0) Exit`nSelect option"
     if($Option -eq 1) {
         #Path configuration
-        $OldPath = (Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).path
-        $NewPath = "$OldPath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
+        $OldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).path
+        $NewPath="$OldPath;$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\"
         Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH -Value $NewPath
         #Winget install condition
-        if (!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\winget.exe")) {
+        if(!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\winget.exe")) {
             Write-Host "`n`n`Winget installation`n"
             Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-            $InstallId = (Get-Process AppInstaller).Id
+            $InstallId=(Get-Process AppInstaller).Id
             Wait-Process -Id $InstallId
         }
         #Windows terminal install condition
-        if (!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe")) {
+        if(!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\wt.exe")) {
             Write-Host "`n`n`tWindows Terminal installation`n"
             winget install --id=Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
         }
         #Powershell install condition
-        if (!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe")) {
+        if(!(Test-Path "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\pwsh.exe")) {
             Write-Host "`n`n`tPowerShell installation`n"
             winget install --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
         }
@@ -46,12 +46,14 @@ do {
         Install-PackageProvider NuGet -Force
         Set-PSRepository PSGallery -InstallationPolicy Trusted
         Install-Module -Name Terminal-Icons -Repository PSGallery
+        break
     } elseif($Option -eq 2) {
         #OhMyPosh theme configuration
         Start-Process "https://ohmyposh.dev/docs/themes"
         $PoshTheme=Read-Host "`nWrite your favourite theme"
         Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
         wt -w 0 nt -NoExit
+        break
     } elseif($Option -eq 3) {
         #OhMyPosh uninstallation
         winget uninstall JanDeDobbeleer.OhMyPosh -s winget
@@ -62,9 +64,11 @@ do {
         winget uninstall --id Microsoft.Powershell --source winget
         #Windows terminal uninstall condition
         winget uninstall --id=Microsoft.WindowsTerminal -e
+        break
+    } elseif($Option -eq 0) {
+        Write-Host "`nExit...`n"
+        Start-Sleep(3)
     } elseif(($Option -ne 0) -and ($Option -ne 1) -and ($Option -ne 2) -and ($Option -ne 3)) {
-        #Error exit
-        Write-Host "`n`n`tPlease select valid option`n"
-        Start-Sleep(5)
+        Write-Host "`nPlease select valid option`n"
     }
 } while($Option -ne 0)

9ad618f1bbf935b04712bb36afeb5f84ecaf55a2
Víctor García <95133973+Qv1ko@users.noreply.github.com>
2023-03-14 18:50:14 +0100

    Update autPosh.ps1

--- autPosh.ps1
+++ autPosh.ps1
@@ -40,7 +40,7 @@ do {
         New-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -ItemType File -Force
         Start-Process "https://ohmyposh.dev/docs/themes"
         $PoshTheme=Read-Host "`nWrite your favourite theme"
-        Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+        Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $PROFILE
         #Icons module installation
         Write-Host "`n`n`tIcon module installation`n"
         Install-PackageProvider NuGet -Force
@@ -51,7 +51,7 @@ do {
         #OhMyPosh theme configuration
         Start-Process "https://ohmyposh.dev/docs/themes"
         $PoshTheme=Read-Host "`nWrite your favourite theme"
-        Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+        Write-Output "oh-my-posh init pwsh --config `"`$env:POSH_THEMES_PATH\$PoshTheme.omp.json`" | Invoke-Expression`nImport-Module -Name Terminal-Icons" | Out-File $PROFILE
         wt -w 0 nt -NoExit
         break
     } elseif($Option -eq 3) {
